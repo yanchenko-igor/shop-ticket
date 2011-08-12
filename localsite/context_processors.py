1112442516f85ad77c45d492888706394d1df0ec
Yanchenko Igor <yanchenko.igor@gmail.com>
2011-08-12 18:37:30 +0300

    added recent product in context ad sow them on index page

--- localsite/context_processors.py
+++ localsite/context_processors.py
@@ -1,4 +1,5 @@
 from product.models import Category
+from product.models import Product
 from product.queries import bestsellers
 from product.views import display_featured
 
@@ -7,6 +8,7 @@ def categories(request):
     ctx = {
         'root_categories': Category.objects.root_categories(),
         'bestsellers' : bestsellers(5),
+        'recent' : Product.objects.recent_by_site()[:6],
         'featured' : display_featured(5, True),
     }
 
