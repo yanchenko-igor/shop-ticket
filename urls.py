a00633dd7559e60acc44f1cc861f700924a5373d
Yanchenko Igor <igor@skif.com.ua>
2011-08-14 04:41:55 +0300

    fix urls

--- urls.py
+++ urls.py
@@ -1,9 +1,3 @@
 from django.conf.urls.defaults import *
 
-from satchmo_store.urls import urlpatterns as satchmo_urls
-
-urlpatterns = patterns('',
-        url('^featured/', 'localsite.views.display_featured', name='localsite_featured'),
-        )
-
-urlpatterns += satchmo_urls
+from satchmo_store.urls import urlpatterns
