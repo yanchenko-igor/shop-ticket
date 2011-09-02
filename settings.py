1e24c95725f5c316cbe0a17eab3567cbee9b23a1
Yanchenko Igor <yanchenko.igor@gmail.com>
2011-09-02 12:03:44 +0300

    tinymce settings updated

--- settings.py
+++ settings.py
@@ -180,7 +180,20 @@ SATCHMO_SETTINGS = {
 
 SKIP_SOUTH_TESTS=True
 
-TINYMCE_DEFAULT_CONFIG = {'theme': "advanced", 'relative_urls': False}
+TINYMCE_DEFAULT_CONFIG = {
+        'mode' : "textareas",
+        'theme' : "advanced",
+        'plugins' : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+        'theme_advanced_buttons1' : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+        'theme_advanced_buttons2' : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+        'theme_advanced_buttons3' : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+        'theme_advanced_buttons4' : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+        'theme_advanced_toolbar_location' : "top",
+        'theme_advanced_toolbar_align' : "left",
+        'theme_advanced_statusbar_location' : "bottom",
+        'theme_advanced_resizing' : True,
+        'relative_urls': False,
+        }
 
 TINYMCE_JS_URL = MEDIA_URL + 'js/tiny_mce/tiny_mce.js'
 TINYMCE_JS_ROOT = MEDIA_ROOT + 'js/tiny_mce'
