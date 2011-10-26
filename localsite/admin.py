622908e525cb7bed9d3b4d1de46bf7fbdbb3cec2
Yanchenko Igor <yanchenko.igor@gmail.com>
2011-10-26 18:01:41 +0300

    added filter by subtype of product

--- localsite/admin.py
+++ localsite/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 from store.localsite.models import *
 from django.contrib.flatpages.admin import FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
 from tinymce.widgets import TinyMCE
 from product.admin import ProductOptions
+from django.utils.translation import ugettext as _
 
 class SeatSectionInline(admin.TabularInline):
     model = SeatSection
@@ -84,7 +86,42 @@ class TinyMCEFlatPageAdmin(FlatPageAdmin):
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, TinyMCEFlatPageAdmin)
 
+
+class TypeOfProductListFilter(SimpleListFilter):
+    title = _('Type of product')
+
+    parameter_name = 'type'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('event', _('Event')),
+            ('ticket', _('Ticket')),
+            (None, _('Other')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'event':
+            return queryset.filter(event__product__isnull=False)
+        elif self.value() == 'ticket':
+            return queryset.filter(ticket__product__isnull=False)
+        elif self.value() == None:
+            return queryset.filter(ticket__product__isnull=True, event__product__isnull=True)
+
+        return queryset
+    def choices(self, cl):
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': self.value() == lookup,
+                'query_string': cl.get_query_string({
+                    self.parameter_name: lookup,
+                }, []),
+                'display': title,
+            }
+
+
 class TinyMCEProductOptions(ProductOptions):
+    list_filter = ('category', 'date_added','active','featured')
+    list_filter += (TypeOfProductListFilter,)
     def formfield_for_dbfield(self, db_field, **kwargs):
         if db_field.name == 'description':
             result = super(TinyMCEProductOptions, self).formfield_for_dbfield(db_field, **kwargs)
