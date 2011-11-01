6e637e563f08ef17b7a45e900bf61c0271e51b91
Yanchenko Igor <yanchenko.igor@gmail.com>
2011-11-01 21:40:21 +0200

    remove unneeded imports from context_processors

--- localsite/context_processors.py
+++ localsite/context_processors.py
@@ -1,7 +1,4 @@
 from product.models import Category
-from product.models import Product
-from localsite.queries import bestsellers
-from product.views import display_featured
 from localsite.forms import SelectCityForm
 from localsite.forms import SelectEventForm
 from localsite.models import Announcement
@@ -12,7 +9,6 @@ def categories(request):
 
     ctx = {
         'root_categories': Category.objects.root_categories(),
-        'recent' : Product.objects.recent_by_site(ticket__product__isnull=True)[:6],
         'select_city_form': SelectCityForm(),
         'select_event_form': SelectEventForm(),
         'announcements': Announcement.objects.active(),
