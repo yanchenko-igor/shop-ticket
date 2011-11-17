bccb4a9d4d12b95b7432a4441895a66a51e0b117
Yanchenko Igor <yanchenko.igor@gmail.com>
2011-11-17 14:00:45 +0200

    fixed bestsellers counter

--- localsite/queries.py
+++ localsite/queries.py
@@ -1,73 +1,7 @@
 from keyedcache import cache_get, cache_set, NotCachedError
 from product.models import Product
-import logging
+from localsite.models import Event
 
-log = logging.getLogger('product.queries')
 
 def bestsellers(count):
-    """Look up the bestselling products and return in a list"""        
-    #TODO
-    return Product.objects.none()
-    #must be rewrited
-    sellers = []
-    cached = False
-    try:
-        pks = cache_get('BESTSELLERS', count=count)
-        if pks:
-            pks = [long(pk) for pk in pks.split(',')]
-            productdict = Product.objects.in_bulk(pks)
-            #log.debug(productdict)
-            for pk in pks:
-                try:
-                    if (int(pk)) in productdict:
-                        key = int(pk)
-                    elif long(pk) in productdict:
-                        key = long(pk)
-                    else:
-                        continue
-                    sellers.append(productdict[key])
-                except ValueError:
-                    pass
-            
-            log.debug('retrieved bestselling products from cache')
-        cached = True    
-    except NotCachedError:
-        pass
-    
-    except ValueError:
-        pass
-
-    if not cached:
-        products = Product.objects.active_by_site(ticket__product__isnull=True)
-        work = []
-        for p in products:
-            p_types = p.get_subtypes()
-            if 'Event' in p_types:
-                ct = 0
-                for t in p.event.tickets.all():
-                    ct += t.product.orderitem_set.count()
-                if ct>0:
-                    work.append((ct, p))
-
-            else:
-                ct = p.orderitem_set.count()
-                if ct>0:
-                    work.append((ct, p))
-        
-        work.sort()
-        work = work[-count:]
-        work.reverse()
-    
-        sellers = []
-        pks = []
-    
-        for p in work:
-            product = p[1]
-            pks.append("%i" % product.pk)
-            sellers.append(product)
-         
-        pks = ",".join(pks)
-        log.debug('calculated bestselling %i products, set to cache: %s', count, pks)
-        cache_set('BESTSELLERS', count=count, value=pks)
-        
-    return sellers
+    return Product.objects.filter(id__in=Event.objects.active().extra(select={'s':'select count(*) from localsite_ticket join shop_orderitem on(localsite_ticket.product_id=shop_orderitem.product_id) where localsite_ticket.event_id = localsite_event.product_id'}).order_by('-s'))[:count]
